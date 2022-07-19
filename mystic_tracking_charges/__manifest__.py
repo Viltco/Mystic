5de8a0ac28847463831b5b1b838ddd93c567666a
smcviltco <75363152+smcviltco@users.noreply.github.com>
2022-07-19 18:23:50 +0500

    a

--- mystic_tracking_charges/__manifest__.py
+++ mystic_tracking_charges/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '14.0.0.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'fleet', 'branch', 'mystic_insurance'],
+    'depends': ['base', 'fleet', 'branch'],
 
     # always loaded
     'data': [
