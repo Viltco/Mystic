0de0f694cabe95550c36051f96c1aaecc71e2375
smcviltco <75363152+smcviltco@users.noreply.github.com>
2022-07-28 11:28:31 +0500

    a

--- res_partner_customization/__manifest__.py
+++ res_partner_customization/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base' , 'hr','contacts' , 'select_multiple_models' ,'de_shift_attendance'],
+    'depends': ['base' , 'hr','contacts' , 'select_multiple_models'],
 
     # always loaded
     'data': [
